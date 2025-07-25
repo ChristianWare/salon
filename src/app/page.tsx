c1f44698384a56189e6554b94b077e85adab0a91
Chris Ware <c.p.ware@hotmail.com>
2025-07-25 15:40:10 -0700

    AboutUsSection done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import AboutUsSection from "@/components/home-page/AboutUsSection/AboutUsSection";
 import FeaturedServices from "@/components/home-page/FeaturedServices/FeaturedServices";
 import Hero from "@/components/home-page/Hero/Hero";
 import ValueProposition from "@/components/home-page/ValueProposition/ValueProposition";
@@ -7,6 +8,7 @@ export default function Home() {
     <main>
       <Hero />
       <ValueProposition />
+      <AboutUsSection />
       <FeaturedServices />
     </main>
   );
---
+++ src/components/home-page/AboutUsSection/AboutUsSection.tsx
@@ -0,0 +1,39 @@
+import LayoutWrapper from "@/components/shared/LayoutWrapper";
+import styles from "./AboutUsSection.module.css";
+import Image from "next/image";
+import Img1 from "../../../../public/images/heroii.jpg";
+
+export default function AboutUsSection() {
+  return (
+    <section className={styles.container}>
+      <LayoutWrapper>
+        <div className={styles.content}>
+          <div className={styles.top}>
+            <h2 className={styles.heading}>Comfort. Quality. Muchacho</h2>
+            <p className={styles.copy}>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nihil
+              similique tenetur possimus excepturi illum quisquam deserunt
+              tempore accusamus alias, fuga porro temporibus, ratione non
+              laborum!
+            </p>
+          </div>
+          <div className={styles.bottom}>
+            <div className={styles.imgContainer}>
+              <Image
+                src={Img1}
+                alt='A dog getting a haircut'
+                className={styles.img}
+                fill
+              />
+              <div className={styles.imgOverlay} />
+              <div className={styles.textContainer}>
+                <h3 className={styles.subHeading}>Modenern Equipment</h3>
+                <p className={styles.copyii}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga nemo repellat et soluta. Aut repellendus sed facilis, saepe commodi molestiae placeat aliquam dolorem ipsa, ea et ab distinctio eligendi odit. Libero unde aperiam aspernatur qui!</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </LayoutWrapper>
+    </section>
+  );
+}
