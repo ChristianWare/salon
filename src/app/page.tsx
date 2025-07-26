0c6478cc541d02c3ac1323dcc1e0f25c5ea4e061
Chris Ware <c.p.ware@hotmail.com>
2025-07-25 22:06:26 -0700

    Consultation done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,6 +16,7 @@ export default function Home() {
       <Experts />
       <FeaturedServices />
       <Consultation />
+      <h2>Footer Here</h2>
     </main>
   );
 }
--- src/components/home-page/Consultation/Consultation.tsx
+++ src/components/home-page/Consultation/Consultation.tsx
@@ -1,15 +1,47 @@
 import LayoutWrapper from "@/components/shared/LayoutWrapper";
 import styles from "./Consultation.module.css";
 import Image from "next/image";
+import Img1 from "../../../../public/images/consultation.jpg";
+import Message from "@/components/icons/Message/Message";
+import FalseButton from "@/components/shared/FalseButton/FalseButton";
+import Corner from "@/components/shared/Corner/Corner";
 
 export default function Consultation() {
   return (
     <section className={styles.container}>
-      <div className={styles.content}>
-        <LayoutWrapper>
-          <h2>yo</h2>
-        </LayoutWrapper>
+      <div className={styles.cornerContainer}>
+        <Corner backgroundColor='backgroundColorWhite' />
       </div>
+      <div className={styles.imgOverlay} />
+      <Image src={Img1} fill alt='' title='' className={styles.img} />
+      <LayoutWrapper>
+        <div className={styles.content}>
+          <div className={styles.formmBox}>
+            <div className={styles.headingIconContainer}>
+              <h2 className={styles.heading}>Get a free consultation</h2>
+              <div className={styles.iconContainer}>
+                <Message className={styles.icon} />
+              </div>
+            </div>
+            <p className={styles.copy}>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nulla
+              sunt voluptas reiciendis hic animi aliquam sequi provident,
+              tenetur neque amet! Autem quo animi tenetur error aut aliquam cum
+              omnis vitae.
+            </p>
+            <div className={styles.fakeForm}>
+              <div className={styles.input}>Name</div>
+              <div className={styles.input}>Email</div>
+            </div>
+            <div className={styles.disclosure}>
+              Lorem ipsum dolor sit amet consectetur.
+            </div>
+            <div className={styles.btnContainer}>
+              <FalseButton btnType='orange' text='Submit' />
+            </div>
+          </div>
+        </div>
+      </LayoutWrapper>
     </section>
   );
 }
