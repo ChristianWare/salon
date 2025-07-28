4a1579080ea1f672e1cc448f01203e9ab1b76da0
Chris Ware <c.p.ware@hotmail.com>
2025-07-28 11:21:28 -0700

    footer start

--- src/components/shared/Footer/Footer.tsx
+++ src/components/shared/Footer/Footer.tsx
@@ -1,9 +1,25 @@
+import Button from "../Button/Button";
 import styles from "./Footer.module.css";
 
 export default function Footer() {
   return (
-    <footer className={styles.container}>
-      <h2>Footer Here</h2>
-    </footer>
+    <div className={styles.container}>
+      <div className={styles.top}>
+        <h2 className={styles.heading}>Pamper Your Pooch at Tailored Tails</h2>
+        <div className={styles.topCopyBtnContainer}>
+          <p className={styles.copy}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad quae
+            consequuntur repudiandae adipisci. Ea voluptatibus atque dicta
+            incidunt praesentium assumenda aut.
+          </p>
+          <div className={styles.btnContainer}>
+            <Button btnType='orange' href='' text='Book a consultation' arrow />
+          </div>
+        </div>
+      </div>
+      <div className={styles.bottom}>
+        <footer className={styles.container}></footer>
+      </div>
+    </div>
   );
 }
