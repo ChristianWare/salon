e9385002d450f52f895357f3a3c4e950d39b2c4c
Chris Ware <c.p.ware@hotmail.com>
2025-07-28 15:04:07 -0700

    consultation updated

--- src/components/home-page/Consultation/Consultation.tsx
+++ src/components/home-page/Consultation/Consultation.tsx
@@ -8,40 +8,42 @@ import Corner from "@/components/shared/Corner/Corner";
 
 export default function Consultation() {
   return (
-    <section className={styles.container}>
-      <div className={styles.cornerContainer}>
-        <Corner backgroundColor='backgroundColorWhite' />
-      </div>
-      <div className={styles.imgOverlay} />
-      <Image src={Img1} fill alt='' title='' className={styles.img} />
-      <LayoutWrapper>
-        <div className={styles.content}>
-          <div className={styles.formmBox}>
-            <div className={styles.headingIconContainer}>
-              <h2 className={styles.heading}>Get a free consultation</h2>
-              <div className={styles.iconContainer}>
-                <Message className={styles.icon} />
+    <div className={styles.parent}>
+      <section className={styles.container}>
+        <div className={styles.cornerContainer}>
+          <Corner backgroundColor='backgroundColorWhite' />
+        </div>
+        <div className={styles.imgOverlay} />
+        <Image src={Img1} fill alt='' title='' className={styles.img} />
+        <LayoutWrapper>
+          <div className={styles.content}>
+            <div className={styles.formmBox}>
+              <div className={styles.headingIconContainer}>
+                <h2 className={styles.heading}>Get a free consultation</h2>
+                <div className={styles.iconContainer}>
+                  <Message className={styles.icon} />
+                </div>
+              </div>
+              <p className={styles.copy}>
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nulla
+                sunt voluptas reiciendis hic animi aliquam sequi provident,
+                tenetur neque amet! Autem quo animi tenetur error aut aliquam
+                cum omnis vitae.
+              </p>
+              <div className={styles.fakeForm}>
+                <div className={styles.input}>Name</div>
+                <div className={styles.input}>Email</div>
+              </div>
+              <div className={styles.disclosure}>
+                Lorem ipsum dolor sit amet consectetur.
+              </div>
+              <div className={styles.btnContainer}>
+                <FalseButton btnType='orange' text='Submit' />
               </div>
             </div>
-            <p className={styles.copy}>
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nulla
-              sunt voluptas reiciendis hic animi aliquam sequi provident,
-              tenetur neque amet! Autem quo animi tenetur error aut aliquam cum
-              omnis vitae.
-            </p>
-            <div className={styles.fakeForm}>
-              <div className={styles.input}>Name</div>
-              <div className={styles.input}>Email</div>
-            </div>
-            <div className={styles.disclosure}>
-              Lorem ipsum dolor sit amet consectetur.
-            </div>
-            <div className={styles.btnContainer}>
-              <FalseButton btnType='orange' text='Submit' />
-            </div>
           </div>
-        </div>
-      </LayoutWrapper>
-    </section>
+        </LayoutWrapper>
+      </section>
+    </div>
   );
 }
