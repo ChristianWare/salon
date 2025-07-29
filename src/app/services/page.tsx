bb593668f0394caac61ca588abf88d79611bc843
Chris Ware <c.p.ware@hotmail.com>
2025-07-29 16:12:25 -0700

    services page done

--- src/app/services/page.tsx
+++ src/app/services/page.tsx
@@ -1,9 +1,15 @@
+import Experts from "@/components/home-page/Experts/Experts";
+import WhyChooseUs from "@/components/home-page/WhyChooseUs/WhyChooseUs";
+import AllServices from "@/components/servicesPage/AllServices/AllServices";
 import ServicesPageIntro from "@/components/servicesPage/ServicesPageIntro/ServicesPageIntro";
 
 export default function ServicesPage() {
   return (
     <main>
       <ServicesPageIntro />
+      <AllServices />
+      <WhyChooseUs />
+      <Experts />
     </main>
   );
 }
