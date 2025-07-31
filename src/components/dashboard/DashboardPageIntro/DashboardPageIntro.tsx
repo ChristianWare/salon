148bbd98cf01b15f2eed51abaeac009ec6f31b59
Chris Ware <c.p.ware@hotmail.com>
2025-07-30 17:40:59 -0700

    Dashboard page updated

--- src/components/dashboard/DashboardPageIntro/DashboardPageIntro.tsx
+++ src/components/dashboard/DashboardPageIntro/DashboardPageIntro.tsx
@@ -4,6 +4,7 @@ import { auth } from "../../../../auth";
 import UserButton from "../UserButton/UserButton";
 import Nav from "@/components/shared/Nav/Nav";
 import Button from "@/components/shared/Button/Button";
+import Dog from "@/components/icons/Dog/Dog";
 
 export default async function DashboardPageIntro() {
   const session = await auth();
@@ -13,16 +14,23 @@ export default async function DashboardPageIntro() {
       <LayoutWrapper>
         <Nav />
         <div className={styles.content}>
-          <h1 className={styles.heading}>
-            {session?.user?.name}&lsquo;s Dashboard
-          </h1>{" "}
-          <p className={styles.copy}>
-            Welcome to your dashboard! Here you can manage your account, view
-            your bookings, and access other features.
-          </p>
-          <div className={styles.btnContainer}>
-            <UserButton />
-            <Button btnType='white' text='Go Home' href='/' />
+          <div className={styles.left}>
+            <h1 className={styles.heading}>
+              {session?.user?.name}&lsquo;s Dashboard
+            </h1>{" "}
+            <p className={styles.copy}>
+              Welcome to your dashboard! Here you can manage your account, view
+              your bookings, and access other features.
+            </p>
+            <div className={styles.btnContainer}>
+              <UserButton />
+              <Button btnType='white' text='Go Home' href='/' />
+            </div>
+          </div>
+          <div className={styles.right}>
+            <div className={styles.dogContainer}>
+              <Dog className={styles.dog} />
+            </div>
           </div>
         </div>
       </LayoutWrapper>
