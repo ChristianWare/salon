ec4d9cc9adf48a1d3e86f6fee2167b3feb283fa3
Chris Ware <c.p.ware@hotmail.com>
2025-07-31 16:34:41 -0700

    AdminKPI Card start

---
+++ src/components/admin/AdminKpiCard/AdminKpiCard.tsx
@@ -0,0 +1,9 @@
+import styles from './AdminKpiCard.module.css'
+
+export default function AdminKpiCard() {
+  return (
+    <div className={styles.container}>
+        <h2>AdminKPI Card</h2>
+    </div>
+  )
+}
