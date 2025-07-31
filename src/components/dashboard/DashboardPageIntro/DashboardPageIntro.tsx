d6835efb514ee33a74fc42db791f161e33958f1e
Chris Ware <c.p.ware@hotmail.com>
2025-07-30 17:18:44 -0700

    DashboardPageIntro updated

--- src/components/dashboard/DashboardPageIntro/DashboardPageIntro.tsx
+++ src/components/dashboard/DashboardPageIntro/DashboardPageIntro.tsx
@@ -1,14 +1,31 @@
+import styles from "./DashboardPageIntro.module.css";
+import LayoutWrapper from "@/components/shared/LayoutWrapper";
 import { auth } from "../../../../auth";
 import UserButton from "../UserButton/UserButton";
-import styles from "./DashboardPageIntro.module.css";
+import Nav from "@/components/shared/Nav/Nav";
+import Button from "@/components/shared/Button/Button";
 
 export default async function DashboardPageIntro() {
   const session = await auth();
 
   return (
     <section className={styles.container}>
-      <h1>{session?.user?.name}&lsquo;s Dashboard</h1>
-      <UserButton />
+      <LayoutWrapper>
+        <Nav />
+        <div className={styles.content}>
+          <h1 className={styles.heading}>
+            {session?.user?.name}&lsquo;s Dashboard
+          </h1>{" "}
+          <p className={styles.copy}>
+            Welcome to your dashboard! Here you can manage your account, view
+            your bookings, and access other features.
+          </p>
+          <div className={styles.btnContainer}>
+            <UserButton />
+            <Button btnType='white' text='Go Home' href='/' />
+          </div>
+        </div>
+      </LayoutWrapper>
     </section>
   );
 }
--- src/components/dashboard/UserButton/UserButton.tsx
+++ src/components/dashboard/UserButton/UserButton.tsx
@@ -2,13 +2,12 @@
 
 import { signOut } from "next-auth/react";
 
-import styles from "./UserButton.module.css";
 import FalseButton from "@/components/shared/FalseButton/FalseButton";
 
 export default function UserButton() {
   return (
-    <div className={styles.container}>
-      <FalseButton btnType="blue" onClick={() => signOut()}>Sign Out</FalseButton>
-    </div>
+    <FalseButton btnType='orange' onClick={() => signOut()}>
+      Sign Out
+    </FalseButton>
   );
 }
